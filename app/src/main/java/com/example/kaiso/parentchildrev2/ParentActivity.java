0d9f291d21e2531787d493542070f5bc6425e89d
Javier Marte <kaison54@hotmail.com>
2016-08-02 14:27:59 -0400

    Made some revisions

--- app/src/main/java/com/example/kaiso/parentchildrev2/ParentActivity.java
+++ app/src/main/java/com/example/kaiso/parentchildrev2/ParentActivity.java
@@ -1,13 +1,20 @@
 package com.example.kaiso.parentchildrev2;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -39,6 +46,14 @@ public class ParentActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent2);
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        final Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                showPopup(v);
+            }
+        });
     }
 
 
@@ -70,30 +85,33 @@ public class ParentActivity extends Activity {
 
     }
 
-    public void check(View view) {
-        if (distance > newRadius) {
-            Context context = getApplicationContext();
-            CharSequence text = "Child out of Zone!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-            //toast out of zone
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "Child in Zone!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-            //toast in zone
-        }
-
-    }
+//    public void check(View view) {
+//        if (distance > newRadius) {
+//            Context context = getApplicationContext();
+//            CharSequence text = "Child out of Zone!";
+//            int duration = Toast.LENGTH_SHORT;
+//
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//            //toast out of zone
+//        } else {
+//            Context context = getApplicationContext();
+//            CharSequence text = "Child in Zone!";
+//            int duration = Toast.LENGTH_SHORT;
+//
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//            //toast in zone
+//        }
+//
+//    }
 
     public void create(View view) {
         EditText userName = (EditText) findViewById(R.id.editText);
         requestData("https://parentchild-6751b.firebaseio.com/users/" + userName.getText() + ".json");
+
+
+
 
     }
 
@@ -173,6 +191,38 @@ public class ParentActivity extends Activity {
 
     }
 
+    public void showPopup(View anchorView) {
+
+        View popupView = getLayoutInflater().inflate(R.layout.fragment_one, null);
+
+        PopupWindow popupWindow = new PopupWindow(popupView,
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        // Example: If you have a TextView inside `popup_layout.xml`
+        TextView tv = (TextView) popupView.findViewById(R.id.textView1);
+
+        tv.setText("heyyyyyyyyyyy");
+
+        // Initialize more widgets from `popup_layout.xml`
+
+
+        // If the PopupWindow should be focusable
+        popupWindow.setFocusable(true);
+
+        // If you need the PopupWindow to dismiss when when touched outside
+        popupWindow.setBackgroundDrawable(new ColorDrawable());
+
+        int location[] = new int[2];
+
+        // Get the View's(the one that was clicked in the Fragment) location
+        anchorView.getLocationOnScreen(location);
+
+        // Using location, the PopupWindow will be displayed right under anchorView
+        popupWindow.showAtLocation(anchorView, Gravity.NO_GRAVITY,
+                location[0], location[1] + anchorView.getHeight());
+
+    }
+
     public void updateDisplay(String x) {
 
         try {
@@ -227,6 +277,8 @@ public class ParentActivity extends Activity {
 
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
+            //showPopup(FragmentOne);
+
             //toast out of zone
         } else {
             Context context = getApplicationContext();
