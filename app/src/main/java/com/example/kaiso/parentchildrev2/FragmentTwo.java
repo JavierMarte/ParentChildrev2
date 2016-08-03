d4ab158aa74a094ed7175bf67e683bf764df5a04
Javier Marte <kaison54@hotmail.com>
2016-08-03 14:06:30 -0400

    Made some revisions

---
+++ app/src/main/java/com/example/kaiso/parentchildrev2/FragmentTwo.java
@@ -0,0 +1,35 @@
+package com.example.kaiso.parentchildrev2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kaiso on 8/2/2016.
+ */
+public class FragmentTwo extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        //Inflate the layout for this fragment
+        View view = inflater.inflate(
+                R.layout.fragment_one, container, false);
+
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getFragmentManager().beginTransaction().remove(FragmentTwo.this).commit();
+            }
+        });
+
+
+        return view;
+    }
+
+
+
+}
